b8a1b9f2fab8ef158e47936bfa70611a9a118753
Neha Nautiyal <nehahaha@gmail.com>
2019-03-11 21:13:14 -0400

    linked files and more

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
 
-    var query = "volcano";
-    var beginYear = 2000;
-    var endYear = 2010;
+    
+    var startYear;
+    var endYear;
+    var query;
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&facet_fields=source&facet=true&begin_date=" + startYear + "0101&end_date=" + endYear + "1231&api-key=gaaUPqq5CREre6dJLXXyMb4vIiMlSK4n";
 
-    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query + "&facet_fields=source&facet=true&begin_date=" + beginYear + "0101&end_date=" + endYear + "1231&api-key=gaaUPqq5CREre6dJLXXyMb4vIiMlSK4n";
 
+
+    $("#submit").on("click", function (event) {
+        // Preventing the button from trying to submit the form
+        event.preventDefault();
+        // Storing the artist name
+        query = $("#search-input").val();
+        startYear = $("#start-year").val();
+        endYear = $("#end-year").val();
+
+        
+     
+console.log(query);
+console.log(startYear);
+console.log(endYear);
 
     $.ajax({
         url: queryUrl,
@@ -30,11 +45,12 @@ $(document).ready(function () {
             $(".articles").append('<br>');
 
 
+
         }
 
 
 
-
+    });
 
     })
 
